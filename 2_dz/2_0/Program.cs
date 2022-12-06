93543196901636a14e634c505881e5cd7bfc3f56
stas <gulfstrm@rambler.ru>
2022-12-07 02:54:04 +0300

    Task 2_0

---
+++ 2_dz/2_0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу , которая выводит случайное число
+// из отрезка [10,99]  и показывает наибольшую цифручисла.
+
+int MaxNumber(int num)
+{
+    Console.WriteLine(num);
+
+    if(num % 10 > num / 10)
+       return num % 10 ;
+    else 
+       return num /10;
+}
+
+Console.WriteLine(MaxNumber(new Random().Next(10,100)));
+
+// 123456 % 10 -> 6
+// 123456 / 10 -> 12345
+// 123456 / 100 -> 1234
+
+ // 12 % -> 2
+ // 12 / -> 1
