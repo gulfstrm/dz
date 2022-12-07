59f0458d08b4c69fa8bc225399c68b16e0f2b340
stas <gulfstrm@rambler.ru>
2022-12-07 03:51:52 +0300

    2_2 кратно некратно

---
+++ 2_dz/2_0/2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу котрая будет принимать на вход два числа
+// и выводить ,является ли второе число кратным первому.
+// Если число 2 не кратно числу 1 ,то программа выводит остаток от деления.
+
+string crat(int num,int num2)
+{
+    if(num % num2 ==0)
+       return"кратно";
+    else
+       return $"некратно,остаток = {num % num2}";
+}
+Console.WriteLine(" 1 число");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine(" 2 число");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine(crat(first,second));
