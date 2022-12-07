27cfbd3cb48294e5c4c9867ff1d5f33e7d680df9
stas <gulfstrm@rambler.ru>
2022-12-07 22:19:32 +0300

    три вход показывает вторую 3_1

---
+++ 2_dz/2_0/3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напиши программу которая принимает на вход трехзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число:  ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+while(n < 100 || n > 999  )
+ {
+    Console.Write("Вы ошиблись !\n Введите трехзначное число:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(n/ 10 % 10);
