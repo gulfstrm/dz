952282762ebd089f0335416313f72ed85caa87c8
stas <gulfstrm@rambler.ru>
2022-12-09 19:11:45 +0300

    3_4 дни недели ипроверяет выходной

--- 2_dz/3_4/Program.cs
+++ 2_dz/3_4/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Напиши программу которая принимает на вход цифру обозначаую
 // день недели и проверяет является ли этот день выходной.
+Console.Write(" Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n<1 || n>7)
+{
+    Console.Write("Вы ошиблись!\nВведитедень недели:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if(n == 6||n == 7)
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");
