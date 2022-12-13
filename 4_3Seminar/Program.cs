780397a92d64a731d6289601d6bbc014c93c9d95
stas <gulfstrm@rambler.ru>
2022-12-13 21:28:34 +0300

    4_3 вход число n выдает * от 1 до n

---
+++ 4_3Seminar/Program.cs
@@ -0,0 +1,13 @@
+﻿// напиши прог которая принимает на вход число n и выдает
+// произведение чисел о  1 до n.
+
+int Proiz(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum = all_sum * i;
+    }
+    return all_sum;
+}
+Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
