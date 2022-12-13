e28d7714ce00d1e1d7e79f77957af25d014b0932
stas <gulfstrm@rambler.ru>
2022-12-13 18:47:25 +0300

    3_2 dz испраленный

--- 2_dz/3_2DZ/Program.cs
+++ 2_dz/3_2DZ/Program.cs
@@ -1,26 +1,25 @@
 ﻿// Напиши программу которая принимает на вход координаты двух точек и находит расстояние
 // между ними в 3D пространстве.
-int ReadNumber(string message)
+ float ReadNumber(string message)
 {
   Console.Write(message);
    var s = Console.ReadLine();
-   int a = s == null ? 0 : int.Parse(s);
+   float a = s == null ? 0 : float.Parse(s);
   return a;
 }
-double Distance(int ax,int ay,int az,int bx,int by,int bz )
+double Distance( float ax,float ay,float az,float bx,float by,float bz )
 {
     double result;
     result = Math.Sqrt(Math.Pow(bx-ax,2)+Math.Pow(by-ay,2)+Math.Pow(bz-az,2));
 
     return result;
 }
+float ax = ReadNumber("Введите ax: ");
+float ay = ReadNumber("Введите ay: ");
+float az = ReadNumber("Введите az: ");
+float bx = ReadNumber("Введите bx: ");
+float by = ReadNumber("Введите by: ");
+float bz = ReadNumber("Введите bz: ");
 
-int ax = ReadNumber("Введите aX:");
-int ay = ReadNumber("Введите aY:");
-int az = ReadNumber("Введите aZ:");
-int by = ReadNumber("Введите bY:");
-int bx = ReadNumber("Введите bX:");
-int bz = ReadNumber("Введите bZ:");
 
-
-Console.WriteLine(Math.Round(Distance(ax,ay,az,bx,by,bz),2));
+Console.WriteLine(Distance( ax, ay, az, bx, by, bz ));
