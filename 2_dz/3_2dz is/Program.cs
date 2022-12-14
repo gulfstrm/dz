b23e11d08c4da17d5518c5e46a1904c24a8cec85
stas <gulfstrm@rambler.ru>
2022-12-14 20:47:59 +0300

    Exampel013_Fibonacci

---
+++ 2_dz/3_2dz is/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напиши программу которая принимает на вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+Console.Clear();
+Console.WriteLine("Введите координату x:");
+double xa = Convert.ToDouble(Console.ReadLine());
+double ya = Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("Введите координату y:");
+ double xb = Convert.ToDouble(Console.ReadLine());
+ double yb = Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("Введите координату z:");
+double xz = Convert.ToDouble(Console.ReadLine());
+double yz = Convert.ToDouble(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(xa-xb,2)+Math.Pow(ya-yb,2)+Math.Pow(xz-yz,2));
+Console.WriteLine($"distance between points: {result}");
---
+++ 4_1 seminar/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу которая принимает на вход число (а) и 
+//выдает сумму чисел от 1 до а.
+
+int SumNums(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum += i;
+    }
+
+    return all_sum;
+}
+
+Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
+
+int i = 0;
+i++;
+i += 1;
+i = i + 1;
+
