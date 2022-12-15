f2f55ac7491e7f518375d37cb1186fd9ce17af7b
stas <gulfstrm@rambler.ru>
2022-12-15 17:34:25 +0300

    4_2 вход число выход сумму цифр в числе

--- 4_2Dz/Program.cs
+++ 4_2Dz/Program.cs
@@ -1,2 +1,14 @@
 ﻿//  Напиши прог которая принимает на вход число и 
 //выдает сумму цифр в числе.
+
+int SumNums(int num)
+{
+    int n_sum = 0;
+    while (num > 0) 
+    {
+        n_sum += num % 10;
+        num /= 10;
+    }
+return n_sum;
+}
+Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
