835c2dbd13002d1fa11629dc0d0b315eec313db5
stas <gulfstrm@rambler.ru>
2022-12-15 19:41:22 +0300

    4_3 задает массив из 8 выход на экран

--- 4_3DZ/Program.cs
+++ 4_3DZ/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Напиши прог которая задает массив из 8 элементов случайными числами  и выводит их на экран
 // Оформите заполнение массива и вывод в виде функции.
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+int[] EightMass(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1, 100);
+    return arr;
+}
+int[] arr_1 = EightMass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+int[] arr_2 = EightMass(int.Parse(Console.ReadLine()));
+Print(arr_2);
