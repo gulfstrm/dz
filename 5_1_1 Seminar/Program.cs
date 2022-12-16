6d879419f4c4a77cba50c8aec0792c2c805cca6b
stas <gulfstrm@rambler.ru>
2022-12-17 01:30:32 +0300

    5_1_2 замена элементов масс +на -

--- 5_1_1 Seminar/Program.cs
+++ 5_1_1 Seminar/Program.cs
@@ -1,28 +1,34 @@
 ﻿// Нпапиши прог замена элементов массива: положительные элементы замените
 // на соответствующие отрицательные и наоборот.
+
 void Print(int[] arr)
 {
     int size = arr.Length;
+
     for (int i = 0; i < size; i++)
         Console.Write($"{arr[i]} ");
     Console.WriteLine();
 }
+
 int[] Mass(int size)
 {
     int[] arr = new int[size];
+
     for (int i = 0; i < size; i++)
         arr[i] = new Random().Next(-100, 100);
     return arr;
 }
 
-
-void Massiv(int[] arr_1)
+string Poisk(int[] arr, int num)
 {
-    for(int i = 0; i < arr_1.Length; i++)
-    arr_1[i] = -arr_1[i];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(num == arr[i])
+        return "yes";
+    }
+    return "no";
 }
 
 int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
 Print(arr_1);
-Massiv(arr_1);
-Print(arr_1);
+Console.WriteLine(Poisk(arr_1,int.Parse(Console.ReadLine())));
