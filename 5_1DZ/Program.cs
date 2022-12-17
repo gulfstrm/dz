52495c2481c65f9154ce6da9c84c413775071f32
stas <gulfstrm@rambler.ru>
2022-12-18 01:24:44 +0300

    масив случ + трех.покажет колво четных

---
+++ 5_1DZ/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите прог которая покажет колво четных чисел в массиве.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
