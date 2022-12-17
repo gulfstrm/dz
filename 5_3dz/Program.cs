56140378d0a2584e12cbcd95066016ebd147a04c
stas <gulfstrm@rambler.ru>
2022-12-18 01:54:30 +0300

    5_3 массив.Разница макс мин.

---
+++ 5_3dz/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив вещественных чисел.Найдите разницу между
+// максимальными и минимальными элементов массива.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+
+int FindMax(int[] array)
+{
+    int max = 0;
+    int min = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        if (array[i] < min)
+            min = array[i];
+    }
+    return max;
+    return min;
+}
+int FindMin(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i] < min)
+            min = array[i];
+    }
+    return min;
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("Разница между мактсмальным и минимальным: ");
+Console.WriteLine(FindMax(array) - FindMin(array));
