506ae20b9b5a9650d2a583ae51db4dc5e335ae38
stas <gulfstrm@rambler.ru>
2022-12-18 01:47:42 +0300

    5_2 зполненный одномерный массив

---
+++ 5_2Dz/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте одномерный массив  заполненный случайными числами .
+// Найдите сумму элементов стоящих на нечетных позициях.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+int ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+            sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
