41699f3ac3a136c99cc57d3c859646ba0d492732
stas <gulfstrm@rambler.ru>
2022-12-18 23:18:04 +0300

    5_3 вход три пров треуг?

---
+++ 5_3Seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напиши прог которая принимает на вход три числа и проверяет
+// может ли сушествовать треугольник со сторонами такой длины.
+
+void TryG()
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int c = int.Parse(Console.ReadLine());
+
+    if (a < b+c & b < a + c & c < a + b)
+    Console.WriteLine(" Yes");
+    else
+    Console.WriteLine("no");
+}
+TryG();
