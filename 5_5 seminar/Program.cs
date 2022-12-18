4a97f90adbe0b30eb329de99d422c4df9993debf
stas <gulfstrm@rambler.ru>
2022-12-19 00:16:25 +0300

    5_5 Фибонначи

---
+++ 5_5 seminar/Program.cs
@@ -0,0 +1,13 @@
+﻿// неиспользую рекурсию выведи первые n число Фибонначи.
+// Первые два числа Фибонначи : 0 и 1.
+void Fibbonacci (int num)
+{
+    int a = 0, b = 1;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"{a} ");
+        (a,b) = (b,a + b);
+    }
+}
+int num = int.Parse(Console.ReadLine());
+Fibbonacci(num);
