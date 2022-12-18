9d00e7e43cfd6be69ff9072823b8c676dd8653e4
stas <gulfstrm@rambler.ru>
2022-12-18 14:09:15 +0300

    учфьзуд5_1

---
+++ Exampel5_1/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+var a = 12;
+Console.WriteLine(a.GetType());
+
+var date = new int[] { 1, 2, 3, 4 }
+         .Where(e => e > 0)
+         .Select(e => new { q = e, w = e + 1 });
+         Console.WriteLine(date.GetType());
