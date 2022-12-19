2f8cf1e5f35ab69e318d882efcc44488f944079b
stas <gulfstrm@rambler.ru>
2022-12-19 19:34:07 +0300

    посмотреть папки

---
+++ 6_8 Exampel/Program.cs
@@ -0,0 +1,12 @@
+﻿// Посмотреть папаки.Обойти дерриторию.
+
+string path = "/Users/gulfs/OneDrive/Рабочий стол/домашки/examples/c_Sharp_Coursee/1_lesson";
+
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
