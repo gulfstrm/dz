389d48d82dfd3e6743303629b25f10ecd27a1870
stas <gulfstrm@rambler.ru>
2022-12-19 18:48:29 +0300

    5 игроков

---
+++ 6_7 Exampel/Program.cs
@@ -0,0 +1,15 @@
+﻿// 5 игроков .Нужно показать все разбиение игроков на 2 команды.0 попадает в 1 команду.
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet,word,length + 1);
+    }
+}
+FindWords("аисв",new char[2]);
