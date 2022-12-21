c1259be1ca5e1462a0e98388304eb030bc4695ae
stas <gulfstrm@rambler.ru>
2022-12-21 23:25:10 +0300

    6_1 ввод М сколько > 0

---
+++ 6_1DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитай сколько чисел больше O ввел пользователь.
+
+int Check(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+{
+    Console.Write($"Число {i}: ");
+    int  a = int.Parse(Console.ReadLine());
+    if (a > 0) count++;
+}
+return count;
+}
+Console.Write("Сколько чисел хотите вставить?: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Колличество чисел больше 0: {Check(a)}");
