fa418dbfa47b2e03aca0fb7512614b41530b5b25
stas <gulfstrm@rambler.ru>
2022-12-24 13:59:52 +0300

    9_2 M и N вывод все натур числа

---
+++ 9_2Seminar/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задай значение M и N .Напиши прог которая рекрсивно
+// выведет все натур числа в промежутке от m до n.
+
+void Numbers(int m,int n)
+{
+    if(m > n) return;
+    Console.Write($"{m} ");
+    Numbers(m + 1,n);
+}
+Numbers(5,8);
