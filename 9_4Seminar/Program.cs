bbcbed56440665e42210dbd95e3d8ae29e4571fe
stas <gulfstrm@rambler.ru>
2022-12-25 01:04:19 +0300

    9_4 вход 2 цифры .1 в степень 2

---
+++ 9_4Seminar/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напиши прогкотрая на вход принимает два числа A и B 
+ // и возводит число A  в целую степень B с помощью рекурсии.
+
+int Degree ( int a,int b)
+{
+    if (b == 0) return 1;
+    return Degree (a, b - 1) * a;
+}
+Console.WriteLine((Degree(3,5)));
