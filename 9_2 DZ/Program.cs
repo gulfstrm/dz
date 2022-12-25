c6c7b7e5172caf130afc1a3ada3c3de6aaf697a4
stas <gulfstrm@rambler.ru>
2022-12-25 19:35:57 +0300

    9_2 M N сумма натур чисел рек

---
+++ 9_2 DZ/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задай значение M и N .Напиши прог котрая найдет сумму натуральных элементов в промежутке 
+// от M до  N c помощью рекурсии.1,5   4,8
+
+  int rec(int m, int n)
+{
+    if (m == n)
+        return m;
+    return rec(m, n - 1) + n;
+}
+
+
+Console.Clear();
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
